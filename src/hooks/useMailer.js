071e0f586ed8fcee2cfc9e3e4d0b001500ff2810
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-17 21:49:13 +0530

    added mailer hook

---
+++ src/hooks/useMailer.js
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+export default async function useMailer({ from, subject, message }) {
+  try {
+    let info = await transporter.sendMail({ from, to: 'akrishnamoorthy007@gmail.com', subject, html: message });
+    return info.messageId;
+  } catch (err) {
+    return err;
+  }
+}
