52f3abae35d41062c7575fffb158f5d6219704a1
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-17 21:27:02 +0530

    added colors

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-function App() {
+export default function Portfolio() {
   return (
     <>
     </>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Portfolio from './Portfolio';
+import './styles/base.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Portfolio />, document.querySelector('#root'));
