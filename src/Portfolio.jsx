dfa699149b1b8e39095c5112ef2b58121e2792f0
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-19 18:11:58 +0530

    setup typography styles

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 export default function Portfolio() {
   return (
     <>
+      <h1>Krishna Moorthy</h1>
+      <h2>Krishna Moorthy</h2>
+      <h3>Krishna Moorthy</h3>
+      <h4>Krishna Moorthy</h4>
+      <h5>Hi, my name is</h5>
+      <h6>Hi, my name is</h6>
+      <p>Lorem ipsum sit amit dollor.</p>
     </>
   );
 }
