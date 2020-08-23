30f18df19c0aff1d8d93b784ca8e69c2c9202e95
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-23 20:26:14 +0530

    added quick links

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from './Components/Button/Button';
 import classes from './styles/styles.module.scss';
 
 export default function Portfolio() {
@@ -9,6 +8,11 @@ export default function Portfolio() {
         <h5 className={classes.intro}>Hi, my name is</h5>
         <h1 className={classes.hero__name}> <span className={classes.emphasize}>Krishna</span> Moorthy</h1>
         <h2 className={classes.info}>UI/UX designer & Full stack developer</h2>
+      </div>
+
+      <div className={classes.quickContact}>
+        <a href='mailto: akrishnamoorthy007@gmail.com'>akrishnamoorthy007@gmail.com</a>
+        <a href='https://twitter.com/krish_the_dev'>twitter/@krish_the_dev</a>
       </div>
     </div>
   );
