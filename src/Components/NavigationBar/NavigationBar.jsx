eab935e7847e97e977fd534a180c93f4af008dcf
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-28 21:17:44 +0530

    added navbar [2/3]

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -7,17 +7,21 @@ export default function NavigationBar() {
   return (
     <nav className={classes.nav}>
       <div className={classes.container}>
-        <ul className={classes.nav__list}>
-          <li className={classes.nav__item}>
-            <a className={classes.nav__link} href='#about'>About</a>
-          </li>
-          <li className={classes.nav__item}>
-            <a className={classes.nav__link} href='#projects'>Projects</a>
-          </li>
-          <li className={classes.nav__item}>
-            <a className={classes.nav__link} href='#contact'>Contact</a>
-          </li>
-        </ul>
+        <div className={classes.nav__list_container}>
+          <input type='checkbox' className={classes.nav__check} id='nav__check' />
+          <label htmlFor='nav__check' className={classes.nav__toggle}>&nbsp;</label>
+          <ul className={classes.nav__list}>
+            <li className={classes.nav__item}>
+              <a className={classes.nav__link} href='#about'>About</a>
+            </li>
+            <li className={classes.nav__item}>
+              <a className={classes.nav__link} href='#projects'>Projects</a>
+            </li>
+            <li className={classes.nav__item}>
+              <a className={classes.nav__link} href='#contact'>Contact</a>
+            </li>
+          </ul>
+        </div>
         <Button link='#resume'>Download CV</Button>
       </div>
     </nav>
