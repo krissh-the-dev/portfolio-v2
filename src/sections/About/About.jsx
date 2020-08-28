7d010553bb0103c4eb93acc6be1000c6117b91b0
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-28 17:33:39 +0530

    added about section [1/3]

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -4,22 +4,25 @@ import classes from './styles.module.scss';
 export default function About() {
   return (
     <section className={classes.about}>
-      <h3 className={classes.heading}>About me</h3>
-      <div className={classes.about__content}>
-        <p className={classes.desciption}>
-          Hello, world! I am Krish, a software engineer from Chennai, India. I build high-quality applications for the web. I concentrate on writing clean and understandable code. Currently, I'm pursuing my undergrads in Computer Science Engineering at University College of Engineering, Trichy. I love to work in a team.
+      <div className={classes.container}>
+        <h3 className={classes.heading}>About me</h3>
+        <div className={classes.about__content}>
+          <p className={classes.description}>
+            Hello, world! I am Krish, a software engineer from Chennai, India. I build high-quality applications for the web. I concentrate on writing clean and understandable code. Currently, I'm pursuing my undergrads in Computer Science Engineering at University College of Engineering, Trichy. I love to work in a team.
              <br />
+            <br />
             Other than coding, I'm passionate about Astronomy and bicycling. I like to cook in my leisure time. I love cats and adore all pet animals.
           </p>
-        <img src='./images/krish.png' />
-      </div>
+          <img className={classes.image} alt='Krish' src='./images/krish.png' />
+        </div>
 
-      <div className={classes.skills}>
+        <div className={classes.skills}>
+          <h4 className={classes.subheading}>Skills</h4>
+        </div>
 
-      </div>
+        <div className={classes.social}>
 
-      <div className={classes.social}>
-
+        </div>
       </div>
     </section>
   );
