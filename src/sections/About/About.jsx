434ddbf702844dc5172eed815a4b225a33460a45
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-30 19:42:46 +0530

    fixes in about section

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -62,7 +62,7 @@ export default function About() {
 
             <span class="logo logo-c"></span>
 
-            <span class="logo logo-cpp"></span>
+            <span class="logo logo-cpp"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span></span>
 
             <span class="logo logo-php"></span>
           </div>
