379c59ea076821d900a50424285cf99d77328319
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-30 11:38:12 +0530

    some more fixes to about section

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -14,10 +14,10 @@ export default function About() {
               <span role='img' aria-label='wave emoji'>👋</span> Hello, world! I am Krish, a software engineer from Chennai, India.
             </p>
             <p className={classes.description}>
-              I build high-quality applications for the web. I concentrate on writing clean and understandable code. Currently, I'm pursuing my undergrads in Computer Science Engineering at University College of Engineering, Trichy. I love to work in a team.
+              I love to design and code <span role='img' aria-label='code emoji'>👩‍💻</span>. I build high-quality applications for the web. I concentrate on writing clean and understandable code. Currently, I'm pursuing my undergrads <span role='img' aria-label='code graduate'>👨‍🎓</span> in Computer Science Engineering at University College of Engineering, Trichy. I love to work in a team.
             </p>
             <p className={classes.description}>
-              I love to design and code <span role='img' aria-label='code emoji'>👩‍💻</span>. Other than coding, I'm passionate about Astronomy <span role='img' aria-label='galaxy emoji'>🌌</span> and bicycling <span role='img' aria-label='cycling emoji'>🚴‍♂️</span>. I like to cook in my leisure time. I love cats <span role='img' aria-label='cat face emoji'>😺</span> and adore all pet animals <span role='img' aria-label='pet dog emoji'>🐕</span>.
+              Other than coding, I'm passionate about Astronomy <span role='img' aria-label='galaxy emoji'>🌌</span> and bicycling <span role='img' aria-label='cycling emoji'>🚴‍♂️</span>. I like to cook in my leisure time. I love cats <span role='img' aria-label='cat face emoji'>😺</span> and adore all pet animals <span role='img' aria-label='pet dog emoji'>🐕</span>.
             </p>
           </div>
           <div className={classes.profile}>
