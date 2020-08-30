95c885cbafebab7ee7fdc7b92114a9f8e78cb58a
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-30 09:29:55 +0530

    about me section [2/4]

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import classes from './styles.module.scss';
 
+import Krish from '../../images/krish.png';
+
 export default function About() {
   return (
     <section className={classes.about}>
       <div className={classes.container}>
         <h3 className={classes.heading}>About me</h3>
         <div className={classes.about__content}>
-          <p className={classes.description}>
-            Hello, world! I am Krish, a software engineer from Chennai, India. I build high-quality applications for the web. I concentrate on writing clean and understandable code. Currently, I'm pursuing my undergrads in Computer Science Engineering at University College of Engineering, Trichy. I love to work in a team.
-             <br />
-            <br />
-            Other than coding, I'm passionate about Astronomy and bicycling. I like to cook in my leisure time. I love cats and adore all pet animals.
-          </p>
-          <img className={classes.image} alt='Krish' src='./images/krish.png' />
+          <div className={classes.about__text}>
+            <p className={classes.description}>
+              <span role='img' aria-label='wave emoji'>👋</span> Hello, world! I am Krish, a software engineer from Chennai, India.
+            </p>
+            <p className={classes.description}>
+              I build high-quality applications for the web. I concentrate on writing clean and understandable code. Currently, I'm pursuing my undergrads in Computer Science Engineering at University College of Engineering, Trichy. I love to work in a team.
+            </p>
+            <p className={classes.description}>
+              I love to design and code <span role='img' aria-label='code emoji'>👩‍💻</span>. Other than coding, I'm passionate about Astronomy <span role='img' aria-label='galaxy emoji'>🌌</span> and bicycling <span role='img' aria-label='cycling emoji'>🚴‍♂️</span>. I like to cook in my leisure time. I love cats <span role='img' aria-label='cat face emoji'>😺</span> and adore all pet animals <span role='img' aria-label='pet dog emoji'>🐕</span>.
+            </p>
+          </div>
+          <div className={classes.profile}>
+            <img className={classes.image} alt='Krish' src={Krish} />
+          </div>
         </div>
 
         <div className={classes.skills}>
