6e716d46a451d50841d851ed40900f6088900120
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-30 14:42:52 +0530

    added skill rack

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -27,6 +27,45 @@ export default function About() {
 
         <div className={classes.skills}>
           <h4 className={classes.subheading}>Skills</h4>
+          <div className={classes.skill_rack}>
+            <span class="logo logo-html"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></span>
+
+            <span class="logo logo-css"><span class="path1"></span><span class="path2"></span></span>
+
+            <span class="logo logo-sass"></span>
+
+            <span class="logo logo-javascript"><span class="path1"></span><span class="path2"></span></span>
+
+            <span class="logo logo-typescript"><span class="path1"></span><span class="path2"></span></span>
+
+            <span class="logo logo-java"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></span>
+
+            <span class="logo logo-git"></span>
+
+            <span class="logo logo-nodejs"></span>
+
+            <span class="logo logo-gradle"></span>
+
+            <span class="logo logo-express"></span>
+
+            <span class="logo logo-react"></span>
+
+            <span class="logo logo-mongodb"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span></span>
+
+            <span class="logo logo-firebase"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></span>
+
+            <span class="logo logo-mysql"></span>
+
+            <span class="logo logo-postgresql"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span></span>
+
+            <span class="logo logo-figma"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span></span>
+
+            <span class="logo logo-c"></span>
+
+            <span class="logo logo-c++"></span>
+
+            <span class="logo logo-php"></span>
+          </div>
         </div>
 
         <div className={classes.social}>
