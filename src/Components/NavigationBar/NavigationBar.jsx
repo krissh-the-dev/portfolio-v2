9c5a4b4c2f5c86c91acd2760ec5a0aae3847eb9e
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-31 20:56:04 +0530

    added nav hide on scroll

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './NavigationBar.module.scss';
 
 import Button from '../Button';
 
 export default function NavigationBar() {
+  const [navStyle, setNavStyle] = useState(0);
+
+  window.onscroll = function (e) {
+    // print "false" if direction is down and "true" if up
+    if (this.scrollY <= 5) {
+      setNavStyle(0);
+    } else if (this.oldScroll > this.scrollY) {
+      setNavStyle(-1);
+    } else {
+      setNavStyle(1);
+    }
+    this.oldScroll = this.scrollY;
+  }
+
+  const topStyle = {
+    boxShadow: 'none',
+  }
+
+  const freeStyle = {
+    transform: 'translateY(-100%)'
+  }
+
+  const visibleStyle = {
+    transform: 'translateY(0)'
+  }
+
+  const navStyles = [
+    visibleStyle, topStyle, freeStyle
+  ];
+
+  // useEffect(() => {
+  //   console.log('State change: ', navStyle);
+  //   setScrollPosition(window.scrollY);
+  // }, [navStyle])
+
   return (
-    <nav className={classes.nav}>
+    <nav className={classes.nav} style={navStyles[navStyle + 1]}>
       <div className={classes.container}>
         <div className={classes.nav__list_container}>
           <input type='checkbox' className={classes.nav__check} id='nav__check' />
