6ea08e30c5fe810ace8e23096f87354ae09a94b3
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-09-05 12:40:21 +0530

    added data for other projects

---
+++ src/sections/OtherProjects/data.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    title: 'YA Image Converter',
+    type: 'Collaborated project',
+    technology: 'Java Swing',
+    description: 'Yet another image converter that converts RGB (Color) image to Grayscale (B&W).',
+    link: 'https://github.com/KrishnaMoorthy12/ya-image-converter/releases/latest',
+    repo: 'https://github.com/KrishnaMoorthy12/ya-image-converter'
+  },
+  {
+    title: 'Leet Code Time Tracker',
+    type: 'Contributed project',
+    technology: 'Chrome Extension',
+    description: 'An extension to track the number of problems solved in LeetCode and time taken to solve it.',
+    link: 'https://chrome.google.com/webstore/detail/leetcode-time-tracker/obcihoolahbncgakepoceagnjnfgghhl?hl=en&authuser=0',
+    repo: 'https://github.com/VarthanV/LeetCode-Time-Tracker'
+  },
+  {
+    title: 'ASUS Z00VD ADB Toolkit',
+    type: 'Featured project',
+    technology: 'BatchScript',
+    description: 'A Command Line Tool for ASUS Zenfone Go (Z00VD) for unlocking, rooting and more...',
+    link: 'https://technologystew.blogspot.com/2018/04/toolwindowsz00vd14-asus-z00vd-adb.html',
+    repo: 'https://github.com/KrishnaMoorthy12/Asus-Z00VD-Toolkit',
+  },
+  {
+    title: 'Nviktous',
+    type: 'Contributed project',
+    technology: 'React JS',
+    description: 'Nviktous is a prototype of an e-commerce app built with React JS, Express and Mongo DB.',
+    link: 'http://nviktous.herokuapp.com/',
+    repo: undefined
+  },
+  {
+    title: 'APK Batch Installer',
+    type: 'Featured Project',
+    technology: 'Java',
+    description: 'Batch/ Bulk android app installer for PC using ADB.',
+    link: 'https://github.com/KrishnaMoorthy12/apk-installer/releases/latest',
+    repo: 'https://github.com/KrishnaMoorthy12/apk-installer'
+  },
+  {
+    title: 'Meow Meow Cat',
+    type: 'Featured Project',
+    technology: 'Java - Android',
+    description: '',
+    link: 'https://github.com/KrishnaMoorthy12/apk-installer/releases/latest',
+    repo: 'https://github.com/KrishnaMoorthy12/apk-installer'
+  },
+  {
+    title: 'Zosime Tech - Home page',
+    type: 'Collaborated Project',
+    technology: 'HTML - SCSS',
+    description: 'A landing page for Zosime Tech, a web service startup.',
+    link: 'https://zosimetech.tk/',
+    repo: undefined
+  },
+]
