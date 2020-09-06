7c6f5121db8e78882c6d465de02d5cf20e01ec2f
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-09-06 17:56:22 +0530

    integrated github api

--- src/sections/Statistics/Statistics.jsx
+++ src/sections/Statistics/Statistics.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './stats.module.scss';
 
+import { getAllStats } from './data';
 import { StatCard as Card } from '../../Components';
 
 export default function Statistics() {
+  const [stats, setStats] = useState({
+    followers: { totalCount: 0 },
+    following: { totalCount: 0 },
+    totalRepositoriesWithContributedCommits: 0,
+    totalContributions: 0,
+    contributionByWeek: new Array(53),
+  });
+  useEffect(() => {
+    // getAllStats().then(res => setStats(res));
+  }, []);
+  useEffect(() => console.log(stats), [stats]);
   return (
     <section className={classes.statistics}>
       <div className={classes.container}>
         <h3 className={classes.heading}>Statistics</h3>
         <div className={classes.contributions}>
-          <Card data={1300} caption={'contributions last year'} />
-          <Card data={100} caption={'contributions last week'} />
+          <Card data={stats.totalContributions} caption={'contributions this year'} />
+          <Card data={stats.contributionByWeek[stats.contributionByWeek.length - 1]} caption={'contributions this week'} />
         </div>
         <div className={classes.stats}>
-          <Card data={46} caption={'contributed repositories'} />
-          <Card data={36} caption={'followers'} />
-          <Card data={43} caption={'following'} />
+          <Card data={stats.totalRepositoriesWithContributedCommits} caption={'contributed repositories'} />
+          <Card data={stats.followers.totalCount} caption={'followers'} />
+          <Card data={stats.following.totalCount} caption={'following'} />
         </div>
       </div>
     </section>
---
+++ src/sections/Statistics/data.js
@@ -0,0 +1,72 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const token = process.env.REACT_APP_TOKEN;
+const query = `query {
+                user(login: "KrishnaMoorthy12") {
+                  followers {
+                    totalCount
+                  }
+                  following {
+                    totalCount
+                  }
+                  contributionsCollection {
+                    totalRepositoriesWithContributedCommits
+                    totalCommitContributions
+                    totalIssueContributions
+                    totalPullRequestContributions
+                    contributionCalendar {
+                      totalContributions
+                      weeks {
+                        contributionDays {
+                            contributionCount
+                        }
+                      }
+                    }
+                  }
+                }
+              }`;
+
+async function fetchGitHubStats() {
+  const headers = { 'Authorization': `bearer ${token}` }
+  const body = { "query": query }
+  const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers })
+  const data = await response.json()
+  return data;
+}
+
+async function getStatsData() {
+  try {
+    const response = await fetchGitHubStats();
+    return response.data.user;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getWeeklyContributionCount(weeks) {
+  let contributionByWeek = [];
+  weeks.forEach(week => {
+    const weekContribution = week.contributionDays.reduce((total, day) => {
+      return { contributionCount: total.contributionCount + day.contributionCount };
+    });
+    contributionByWeek.push(weekContribution.contributionCount);
+  });
+  return contributionByWeek;
+}
+
+async function getAllStats() {
+  const { followers, following, contributionsCollection } = await getStatsData();
+
+  const { totalRepositoriesWithContributedCommits, totalCommitContributions, totalIssueContributions, totalPullRequestContributions, contributionCalendar } = contributionsCollection;
+
+  const { totalContributions, weeks } = contributionCalendar;
+
+  const contributionByWeek = getWeeklyContributionCount(weeks);
+  return {
+    followers, following, totalRepositoriesWithContributedCommits, totalCommitContributions, totalIssueContributions, totalPullRequestContributions, totalContributions,
+    contributionByWeek
+  }
+}
+
+export { getAllStats }
