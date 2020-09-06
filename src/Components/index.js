beadb200cf5560595cea58c0d9a730f1a8c64f0c
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-09-06 21:22:24 +0530

    added input component & a few vars

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as StatCard } from './StatCard';
+export { default as Input } from './Input';
 export { default as Link } from './Link';
 export { default as NavigationBar } from './NavigationBar';
 export { default as OtherProject } from './OtherProjectRow';
--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import classes from './contact.module.scss';
 
+import { Input } from '../../Components';
+
 export default function Contact() {
   return (
     <section className={classes.contact}>
       <div className={classes.container}>
-        <h6 className={classes.sub}>Contact me</h6>
+        <h6 className={classes.subheading}>Contact me</h6>
         <h3 className={classes.heading}>Get in touch</h3>
-
+        <form className={classes.contact__form}>
+          <Input name={'Name'} type={'text'} />
+          <Input name={'Email'} type={'email'} />
+          <Input name={'Message'} component={'textarea'} />
+        </form>
       </div>
     </section>
   );
