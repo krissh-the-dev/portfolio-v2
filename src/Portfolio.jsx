7c89f293edd0a9a87216eff5fc185e1b6b5c11c5
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-09-12 20:24:47 +0530

    added loader

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Loader, Hero, About, Projects, OtherProjects, Statistics, Contact, Footer } from './sections';
 import { NavigationBar } from './Components';
 
 export default function Portfolio() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+
   return (
     <>
-      <Loader />
+      <Loader style={isLoading ? {} : { opacity: 0, visibility: 'hidden' }} />
       <NavigationBar />
       <main>
         <Hero />
--- src/sections/Loader/Loader.jsx
+++ src/sections/Loader/Loader.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import classes from './loader.module.scss';
 
-export default function Loader() {
+export default function Loader({ style }) {
   return (
-    <div className={classes.loader} style={{ display: 'none' }}>
+    <div className={classes.loader} style={style}>
       <div className={classes.container}>
         <div className={classes.logo}></div>
         <div className={classes.progress}></div>
