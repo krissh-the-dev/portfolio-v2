b5a37747e4f17b4b76feca57fe0eba5af6afbbf0
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-09-13 20:28:00 +0530

    improved hero animations

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import classes from './hero.module.scss';
-import Fade from 'react-reveal/Fade';
 import { Link } from '../../Components';
 
 export default function Hero() {
   return (
     <section className={classes.hero}>
       <div className={classes.container}>
-        <Fade clear delay={4000}>
-          <h5 className={classes.intro}>Hi, my name is</h5>
-          <h1 className={classes.hero__name}>
-            <span className={classes.emphasize}>Krishna</span><span> Moorthy</span></h1>
-          <h2 className={classes.info}>UI/ UX designer & Full stack developer</h2>
-        </Fade>
+        <h5 className={classes.intro}>Hi, my name is</h5>
+        <h1 className={classes.hero__name}>
+          <span className={classes.emphasize}>Krishna</span><span> Moorthy</span></h1>
+        <h2 className={classes.info}>UI/ UX designer & Full stack developer</h2>
       </div>
 
       <div role='img' className={classes.waterMark}>DEV</div>
-
 
       <div className={classes.quickContact}>
         <Link to='mailto: akrishnamoorthy007@gmail.com' lone>akrishnamoorthy007@gmail.com</Link>
