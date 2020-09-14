0e2191cd335d0c587f883c12dcb0d1b7873161a5
Krish M <akrishnamoorthy007@gmail.com>
2020-09-14 10:52:00 +0530

    UX improvements (#11)
    * improved ham menu
    
    * fixed manifest icon links and theme colors
    
    * added cta in stats section

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import classes from './NavigationBar.module.scss';
 import { Fade } from 'react-reveal';
 
@@ -24,6 +24,12 @@ export default function NavigationBar() {
 
   useEffect(() => setNavStyle({ boxShadow: 'none', paddingBottom: '1.5rem', background: 'none' }), []);
 
+  const check = useRef(null);
+
+  const handleHamClose = () => {
+    check.current.checked = false;
+  }
+
   return (
     <nav className={classes.nav} style={navStyle}>
       <Fade top delay={4000} duration={500}>
@@ -34,21 +40,26 @@ export default function NavigationBar() {
           </a>
 
           <div className={classes.nav__list_container}>
-            <input type='checkbox' className={classes.nav__check} id='nav__check' />
+            <input ref={check} type='checkbox' className={classes.nav__check} id='nav__check' />
             <label htmlFor='nav__check' className={classes.nav__toggle}>&nbsp;</label>
 
             <ul className={classes.nav__list}>
               <li className={classes.nav__item}>
-                <a className={classes.nav__link} href='#about'>About</a>
+                <a className={classes.nav__link} onClick={handleHamClose} href='#about'>About</a>
               </li>
               <li className={classes.nav__item}>
-                <a className={classes.nav__link} href='#projects'>Projects</a>
+                <a className={classes.nav__link} onClick={handleHamClose} href='#projects'>Projects</a>
               </li>
               <li className={classes.nav__item}>
-                <a className={classes.nav__link} href='#statistics'>Statistics</a>
+                <a className={classes.nav__link} onClick={handleHamClose} href='#statistics'>Statistics</a>
               </li>
               <li className={classes.nav__item}>
-                <a className={classes.nav__link} href='#contact'>Contact</a>
+                <a className={classes.nav__link} onClick={handleHamClose} href='#contact'>Contact</a>
+              </li>
+
+              <li className={classes.hamCloseBtn} onClick={handleHamClose} >
+                <span className="icon icon-arrow-thin-right"></span>
+                Close
               </li>
             </ul>
           </div>
