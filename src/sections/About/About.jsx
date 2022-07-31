4d4e6a965d650f0b96c370aeaac8064c937a0332
Krish <akrishnamoorthy007@gmail.com>
2022-07-31 23:02:18 +0530

    updated about information

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -19,24 +19,27 @@ export default function About() {
                 <span role='img' aria-label='wave emoji'>
                   👋
                 </span>{' '}
-                Hello, world! I am Krish, a full-stack engineer from Chennai, India.
+                Hello, world! I am Krish, a Professional Sofware Engineer from Chennai, India.
               </p>
               <p className={classes.description}>
-                I love to design and code{' '}
+                I love to code and design{' '}
                 <span role='img' aria-label='code emoji'>
                   👨🏼‍💻
                 </span>
-                . I build high-quality applications for the web. I concentrate on writing clean, understandable and
-                testable code.
+                . I build high-quality applications for the web. I concentrate on writing clean,
+                maintainable and testable code.
               </p>
               <p className={classes.description}>
-                Currently, I'm pursuing my undergrads{' '}
+                Currently, I'm working as a Software Developer at{' '}
+                <Link to='https://zoho.com'>Zoho Corporation Pvt. Ltd.</Link> I pursued my
+                undergrads{' '}
                 <span role='img' aria-label='code graduate'>
                   👨‍🎓
                 </span>{' '}
                 in Computer Science Engineering at the{' '}
-                <Link to='http://aubit.edu.in'>University College of Engineering, Trichy</Link>. I love working in and
-                as a team. I'm always looking for opportunities to learn and grow.
+                <Link to='http://aubit.edu.in'>University College of Engineering, Trichy</Link>. I
+                love working in and as a team. I'm always looking for new opportunities to learn and
+                grow.
               </p>
               <p className={classes.description}>
                 Other than coding, I'm passionate about Astronomy{' '}
