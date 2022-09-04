8735cf3600ed03ca6780f23425f2faf6058d63c5
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-09-04 18:08:26 +0530

    fixed eslint errors

--- src/sections/Projects/data.js
+++ src/sections/Projects/data.js
@@ -36,7 +36,7 @@ const tss = {
     dark: { small: TheSecretStore, large: TheSecretStore }
   },
   description:
-    "The Secret Store is a platform to share and synchronize your project configuration environment variables with your team without having to send a bunch of e-mails back and forth with simple commands like in git. It's Forever FOSS (Free and Open Source Software). You can fork and create your copy, and utilize it for your personal / internal use within your organization.",
+    'The Secret Store is a platform to share and synchronize your project configuration environment variables with your team without having to send a bunch of e-mails back and forth with simple commands like in git. You can fork and create your copy, and utilize it for your personal / internal use within your organization.',
   features: [
     'Command Line Interface',
     'AES 256 Encryption',
